0396199c0658f57e9dfb5a8f9d6efdbc7ca974c6
PC <PC@DESKTOP-26A9125>
2023-07-19 23:25:22 +0530

    Selenium Demo

---
+++ src/testcases/Demo.java
@@ -0,0 +1,18 @@
+package testcases;
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number=10;
+		for (int i = 1; i <= number; i++) {
+
+			
+			System.out.println(i);
+			
+		}
+	}
+
+}
